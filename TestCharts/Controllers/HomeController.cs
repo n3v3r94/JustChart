370e14a2a54617feafa1384e5466b8b3b9d01315
Hristo Ivanovski <ivanovskihristo34@gmail.com>
2019-11-30 22:48:49 +0200

    clean code from unused code

--- TestCharts/Controllers/HomeController.cs
+++ TestCharts/Controllers/HomeController.cs
@@ -11,55 +11,11 @@ namespace TestCharts.Controllers
     {
        
 
-        public async Task<IActionResult> Index()
+        public  async Task<IActionResult> Index()
         {
-       
-           
-
-            return View();
+            return  View();
         }
 
-        //public async Task<string> GetDataChart()
-        //{
-        //    //var jsonConfig = await homeService.GetDataJson();
-        //    //var response = await homeService.GetDataFromApiPost(jsonConfig);
-        //    //var data = await homeService.DataRetrievalResponse(response,jsonConfig);
-        //    //return data;
-        //}
-
-        //public async Task<IActionResult> GetJsonConfiguration()
-        //{
-        //    //var config = await homeService.GetDataJson();
-
-        //    //var test = config;
-        //    //var test2 = config;
-        //    /return Json(config);
-        //}
-
-        public IActionResult GetDataAPI()
-        {
-
-
-            List<ViewModelAPI> chartData = new List<ViewModelAPI>();
-            chartData.Add(new ViewModelAPI
-            {
-                ChartId = "myChartAPI",
-                Data = new List<int> { 12, 9, 3, 5, 3 },
-                Labels = new List<string> { "Red", "Blue", "Yellow", "Green", "Orange" },
-                LabelTitle = "Test"
-            });
-
-            return Json(chartData);
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    
     }
 }
